2acdbe0e26fc6ef36c47bb50b95d67bfcc3597fe
Abhiram-Agina <50762306+Abhiram-Agina@users.noreply.github.com>
2023-05-07 14:24:15 -0700

    Create QuoteGeneratorBasic.py

---
+++ QuoteGeneratorBasic.py
@@ -0,0 +1,15 @@
+
+import random
+import streamlit as st
+import pandas as pd
+
+file = "FILES/Shakespeare_Insults - quotes.csv"
+df = pd.read_csv(file)
+
+if st.button('Generate an Insult'):
+  rand_num = random.randint(1, (df.shape[0] - 1))
+  
+  st.write("Topic: ", str(df.iat[rand_num, 1]).upper())
+  st.write("Line: ", df.iat[rand_num, 2])
+  st.write("Act: ", df.iat[rand_num, 3])
+  st.write("Scene: ", str(df.iat[rand_num, 4]))
