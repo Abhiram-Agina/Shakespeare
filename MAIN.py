f4df452b7c601e5400c72aede97af201b0c52e3d
Abhiram-Agina <50762306+Abhiram-Agina@users.noreply.github.com>
2023-05-07 03:10:33 -0700

    Update MAIN.py

--- MAIN.py
+++ MAIN.py
@@ -1,10 +1,9 @@
 import streamlit as st
 import pandas as pd
-from PIL import Image
 
-st.title("Homepage: **Shakespeare - Natural Language Processing**")
+st.title("**Shakespeare - Natural Language Processing**")
 
-st.header("*Team Analysis*")
+st.header("*Apps*")
 st.markdown(
 '''
 * [Word Cloud Generator](https://abhiram-agina-shakespeare-wordcloud-vbjvi0.streamlit.app/)
