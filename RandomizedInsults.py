a74762e2e91338f3bb3b54e4ea5d7da86e8a3508
Abhiram-Agina <50762306+Abhiram-Agina@users.noreply.github.com>
2023-05-07 14:39:28 -0700

    Update RandomizedInsults.py

--- RandomizedInsults.py
+++ RandomizedInsults.py
@@ -2,13 +2,18 @@ import random
 import streamlit as st
 import pandas as pd
 
-insultAdverbs = "FILES/Shakespeare_Insults - quotes.csv"
-df = pd.read_csv(file)
+insultAdverbs = "FILES/adverbList.txt"
+adverbs = pd.read_csv(insultAdverbs, sep = ' ')
 
-if st.button('Generate a Quote'):
-  rand_num = random.randint(1, (df.shape[0] - 1))
-  
-  st.write("Topic: ", str(df.iat[rand_num, 1]).upper())
-  st.write("Line: ", df.iat[rand_num, 2])
-  st.write("Act: ", df.iat[rand_num, 3])
-  st.write("Scene: ", str(df.iat[rand_num, 4]))
+insultAdjectives = "FILES/adjectiveList.txt"
+adjectives = pd.read_csv(insultAdjectives, sep = ' ')
+
+insultNouns = "FILES/nounList.txt"
+nouns = pd.read_csv(insultNouns, sep = ' ')
+
+
+if st.button('Generate a Wacky Shakespearean Insult'):
+  rand_num1 = random.randint(1, (adverbs.shape[0] - 1))
+  rand_num2 = random.randint(1, (adjectives.shape[0] - 1))
+  rand_num3 = random.randint(1, (nouns.shape[0] - 1))
+  st.write("Thou ", str(adverbs.iat[rand_num, 0]).upper(), str(adjectives.iat[rand_num, 0]).upper(), str(nouns.iat[rand_num, 0]).upper())
