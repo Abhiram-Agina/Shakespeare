d2c5383e938fbbb57bbb7cfecaa2efb1337472d5
Abhiram-Agina <50762306+Abhiram-Agina@users.noreply.github.com>
2023-05-07 03:06:34 -0700

    Update MAIN.py

--- MAIN.py
+++ MAIN.py
@@ -1,1 +1,13 @@
+import streamlit as st
+import pandas as pd
+from PIL import Image
 
+st.title("Homepage: **Shakespeare - Natural Language Processing**")
+
+st.header("*Team Analysis*")
+st.markdown(
+'''
+* [Word Cloud Generator](https://abhiram-agina-shakespeare-wordcloud-vbjvi0.streamlit.app/)
+* [Filtering Shakespeare](https://abhiram-agina-shakespeare-filterworks-uzv0jn.streamlit.app/)
+'''
+)
