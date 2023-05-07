22457213b558cc6c33e81a78a8606bd6adca3a73
Abhiram-Agina <50762306+Abhiram-Agina@users.noreply.github.com>
2023-05-07 14:21:12 -0700

    Update InsultGeneratorBasic.py

--- InsultGeneratorBasic.py
+++ InsultGeneratorBasic.py
@@ -9,6 +9,6 @@ df = pd.read_csv(file)
 if st.button('Generate an Insult'):
   rand_num = random.randint(1, (df.shape[0] - 1))
   
-  st.write("Topic: ", df.iat[rand_num, 1])
+  st.write("Topic: ", str(df.iat[rand_num, 1])).upper())
   st.write("Line: ", df.iat[rand_num, 2])
   st.write("Act: ", df.iat[rand_num, 3], "Scene: ", str(df.iat[rand_num, 4]))
