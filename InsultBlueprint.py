60a5d064771306f48e9c92797b7e41188cba10fe
Abhiram-Agina <50762306+Abhiram-Agina@users.noreply.github.com>
2023-05-07 11:47:11 -0700

    Create InsultBlueprint.py

---
+++ InsultBlueprint.py
@@ -0,0 +1,35 @@
+import functools
+import random
+import sys
+
+
+def applyto(*args):
+    return functools.reduce(lambda l, r: r(l), args)
+
+
+def read_file(filename):
+    content = None
+    with open(filename) as fin:
+        content = [line.rstrip('\n') for line in fin]
+    return content
+
+
+def combine(*args):
+    return [random.choice(arg) for arg in args]
+
+
+def format_insult(lst):
+    return ' '.join(['Thou'] + lst) + '!'
+
+
+def insult(*args):
+    return applyto(
+        [read_file(it) for it in list(args)],
+        lambda content: combine(*content),
+        format_insult
+     )
+
+if __name__ == "__main__":
+    filenames = ['w1.txt', 'w2.txt', 'w3.txt']
+    filenames = sys.argv[1:] or filenames
+    print(insult(*filenames))
