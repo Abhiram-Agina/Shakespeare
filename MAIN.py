fc440b62ad9354cf339e8adebaa832be1fed1d0b
Abhiram-Agina <50762306+Abhiram-Agina@users.noreply.github.com>
2023-05-07 14:26:50 -0700

    Update MAIN.py

--- MAIN.py
+++ MAIN.py
@@ -13,5 +13,7 @@ st.markdown(
 * [Word Cloud Generator](https://abhiram-agina-shakespeare-wordcloud-vbjvi0.streamlit.app/)
 * [Filtering Shakespeare](https://abhiram-agina-shakespeare-filterworks-uzv0jn.streamlit.app/)
 * [Sentiment Detection](https://abhiram-agina-shakespeare-sentimentdetectionl-dt0usp.streamlit.app/)
+* [Basic Insult Generator](https://abhiram-agina-shakespeare-insultgeneratorbasic-9povu7.streamlit.app/)
+* [Basic Quote Generator](https://abhiram-agina-shakespeare-quotegeneratorbasic-ltfk2v.streamlit.app/)
 '''
 )
