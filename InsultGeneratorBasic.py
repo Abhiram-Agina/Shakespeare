3d65ff03f4c28a978bab8964415b9fc3bfa1cd33
Abhiram-Agina <50762306+Abhiram-Agina@users.noreply.github.com>
2023-05-07 13:06:16 -0700

    Update and rename InsultGenerator.py to InsultGeneratorBasic.py

---
+++ InsultGeneratorBasic.py
@@ -0,0 +1,13 @@
+
+import random
+import streamlit as st
+import pandas as pd
+
+st.write("Hello")
+
+file = "Shakespeare_data.csv"
+df = pd.read_csv(file)
+
+if st.button('Random Player Line'):
+  rand_num = random.randint(1,100000)
+  st.write(df.iat[rand_num, 5])
