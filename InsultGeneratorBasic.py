c3d1b3eb8daaaf2e83bad228847ee26ab9b470fe
Abhiram-Agina <50762306+Abhiram-Agina@users.noreply.github.com>
2023-05-07 13:24:59 -0700

    Update InsultGeneratorBasic.py

--- InsultGeneratorBasic.py
+++ InsultGeneratorBasic.py
@@ -9,5 +9,6 @@ file = "FILES/Shakespeare_Insults - insults.csv"
 df = pd.read_csv(file)
 
 if st.button('Random Player Line'):
+  st.write(df.shape[0])
   rand_num = random.randint(1, df.shape[0])
   st.write(df.iat[rand_num, 5])
