d453329cae4a52571f773aa1d4eb9be90de74aa6
Abhiram-Agina <50762306+Abhiram-Agina@users.noreply.github.com>
2023-05-07 17:46:35 -0700

    Update MAIN.py

--- MAIN.py
+++ MAIN.py
@@ -8,6 +8,7 @@ st.write(":red[Shakespeare - Natural Language Processing]")
 st.write('''
 I am interested in Data Science, particularly sports analytics and natural language processing. 
 This is my attempt at exploring Natural Language Processing using the Works of Shakespeare.
+~ Abhiram Agina
 ''')
 
 
