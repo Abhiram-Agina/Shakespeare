586da86a921f9daea8d43233065f8625ced9cee7
Abhiram-Agina <50762306+Abhiram-Agina@users.noreply.github.com>
2023-05-07 23:03:48 -0700

    Update InifiniteMonkeys.py

--- InifiniteMonkeys.py
+++ InifiniteMonkeys.py
@@ -13,3 +13,41 @@ shakespeare = open(path_to_file, 'rb').read() #the entire corpus is now accessib
 shakespeare = shakespeare.decode(encoding='utf-8')
 st.write('Total number of characters in the corpus is:', len(shakespeare)) # returns 178981 total characters
 st.write('The first 500 characters of the corpus are as follows:\n', shakespeare[:500])
+
+st.write("---")
+
+# list of all characters in the book
+typewriter = ["a","b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", ",", "!", "&", "?", "[", "]", " "]
+
+# bestMatches stores the longest string of characters the monkey types that match Romeo & Juliet
+bestMatches = [""]
+
+# stores current string being typed by the monkey
+currentRun = ""
+
+# stores every single character the monkey typed
+totalRun = ""
+
+# total # of characters typed by the monkey
+characterTotal = 0
+
+import random
+
+for x in range(0, 100000): #monkey types 100000 random characters; increase range as needed;
+  newChar = random.choice(typewriter)
+  currentRun += newChar
+  totalRun += newChar
+  characterTotal += 1
+
+  if currentRun in romeo: # if our currentRun IS somewhere in romeo
+    if len(currentRun) > len(bestMatches[0]): # if this is the longest match made so far, then reset the bestMatches to currentRun
+      bestMatches = [currentRun]
+      #st.write(bestMatches, "characters typed: {}".format(characterTotal))
+    elif len(currentRun) == len(bestMatches[0]): # if this is the same lenght as the longest match made so far, then append the currentRun to bestMatches
+      bestMatches.append(currentRun) 
+      #st.write(bestMatches, "characters typed: {}".format(characterTotal))
+  else:
+    currentRun = "" # if our currentRun is NOT anywhere in romeo, then discard the currentRun and start over
+
+# Once the above code run is complete, then print the bestMatches
+st.write(bestMatches, "characters typed: {}".format(characterTotal)) 
