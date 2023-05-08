1fda8b6ad65301206aa9c3097c972a167cda8d35
Abhiram-Agina <50762306+Abhiram-Agina@users.noreply.github.com>
2023-05-07 22:22:59 -0700

    Update AIPlaywright.py

--- AIPlaywright.py
+++ AIPlaywright.py
@@ -14,7 +14,7 @@ path_to_file = tf.keras.utils.get_file('shakespeare.txt', 'https://storage.googl
 # Read the file, then decode for py2 compat.
 text = open(path_to_file, 'rb').read() #the entire corpus is now accessible via the variable named 'text'
 text = text.decode(encoding='utf-8')
-st.write('Total number of characters in the corpus is:', len(text)) # returns 1115394
+# st.write('Total number of characters in the corpus is:', len(text)) # returns 1115394
 st.write('The first 100 characters of the corpus are as follows:\n', text[:100])
 
 # Vectorize Text: STEP 1
@@ -25,13 +25,21 @@ st.write('The first 100 characters of the corpus are as follows:\n', text[:100])
 
 # The unique characters in the corpus
 vocab = sorted(set(text)) #set(text) function returns a set of unique characters in the text file
-st.write('The number of unique characters in the corpus is', len(vocab)) # returns 65 unique characters i.e. 26 alphabets, 10 numbers, punctuation, and special characters.
-st.write('unique characters set:\n', vocab[:]) #prints all 65 unique characters 
+#st.write('The number of unique characters in the corpus is', len(vocab)) # returns 65 unique characters i.e. 26 alphabets, 10 numbers, punctuation, and special characters.
+#st.write('unique characters set:\n', vocab[:]) #prints all 65 unique characters 
 
 # NOTE: in the above approach, RNN uses characters to generate new text. You could also create a vocab of unique words using the code below:
 import re
 words = re.findall('\w+', text.lower())
 uniq_words = sorted(set(words)) #set() finds unique words; sorted() converts the set into a sorted array
 len(uniq_words) #11,456 unique words
-st.write(uniq_words[:10]) # returns first 10 words from the 11,456 unique words
+#st.write(uniq_words[:10]) # returns first 10 words from the 11,456 unique words
 
+# Vectorize Text: STEP 2
+
+# Create a mapping from unique characters to indices
+char2idx = {u:i for i, u in enumerate(vocab)} # creates a dictionary of the set items with their index # to vectorize (encode the text)
+# Make a copy of the unique set elements in NumPy array format for later use in the decoding the predictions
+idx2char = np.array(vocab) #to de-vectorize (decode the text)
+# Vectorize the text with a for loop
+text_as_int = np.array([char2idx[c] for c in text]) # creates a vectorized numpy
