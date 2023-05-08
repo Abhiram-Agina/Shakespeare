05d28b88a77774a13f607ff42930ad221ce91923
Abhiram-Agina <50762306+Abhiram-Agina@users.noreply.github.com>
2023-05-07 17:11:58 -0700

    Update MAIN.py

--- MAIN.py
+++ MAIN.py
@@ -5,8 +5,23 @@ from PIL import Image
 st.title('Bard vs Bot')
 st.subheader("Shakespeare - Natural Language Processing")
 
-shakespearePortraitPic = Image.open("IMAGES/ShakespearePortrait.jpeg")
-st.image(shakespearePortraitPic, width = 300)
+# to get different images in the rows and columns, have a systematic way to label your images. For mine, I have used row_{i}_col_{j}
+
+cols = st.columns(3)
+
+cols[0].image('images/row_0_col_0.png', caption='Search Plays', use_column_width=True)
+cols[1].image('images/row_0_col_1.png', caption='Statistical Analysis', use_column_width=True)
+cols[2].image('images/row_0_col_2.png', caption='Sentiment Analysis', use_column_width=True)
+
+cols[0].image('images/row_1_col_0.png', caption='AI Play Writer', use_column_width=True)
+cols[1].image('images/row_1_col_1.png', caption='Shakespeare', use_column_width=True)
+cols[2].image('images/row_1_col_2.png', caption='Sonnet Clouds', use_column_width=True)
+
+cols[0].image('images/row_2_col_0.png', caption='Infinite MonkRandom', use_column_width=True)
+cols[1].image('images/row_2_col_1.png', caption='Random Insults & Quotes', use_column_width=True)
+cols[1].markdown("[Basic Insult Generator](https://abhiram-agina-shakespeare-insultgeneratorbasic-9povu7.streamlit.app/)")
+cols[2].image('images/row_2_col_2.png', caption='Text Summary', use_column_width=True)
+cols[2].markdown("[Text Summary](https://tulasi-agina-searchshakespeareplays-main-fpxl6w.streamlit.app/)")
 
 st.header("*Apps*")
 st.markdown(
