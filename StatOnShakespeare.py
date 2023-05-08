938b7ca17e8c24a532c04c577635292a5d057a19
Abhiram-Agina <50762306+Abhiram-Agina@users.noreply.github.com>
2023-05-08 01:21:57 -0700

    Create StatOnShakespeare.py

---
+++ StatOnShakespeare.py
@@ -0,0 +1,13 @@
+# StatOnShakespeare.py
+
+import numpy as np
+import pandas as pd
+import streamlit as st
+import altair as alt
+from wordcloud import WordCloud, STOPWORDS
+import glob, nltk, os, re
+from nltk.corpus import stopwords 
+import nltk
+nltk.download('punkt')
+import string
+nltk.download('stopwords')
