646924b2f6519f8a6182a3c0d0c19c3740cdd0da
Abhiram-Agina <50762306+Abhiram-Agina@users.noreply.github.com>
2023-05-07 17:49:29 -0700

    Update MAIN.py

--- MAIN.py
+++ MAIN.py
@@ -11,7 +11,7 @@ This is my attempt at exploring Natural Language Processing using the Works of S
 ~ Abhiram Agina
 ''')
 
-st.write()
+st.divider()
 
 
 # to get different images in the rows and columns, have a systematic way to label your images. For mine, I have used row_{i}_col_{j}
