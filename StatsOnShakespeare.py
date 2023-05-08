6686de04b0267fb4a45aa84513d5a9af158cda9d
Abhiram-Agina <50762306+Abhiram-Agina@users.noreply.github.com>
2023-05-08 01:56:59 -0700

    Update StatsOnShakespeare.py

--- StatsOnShakespeare.py
+++ StatsOnShakespeare.py
@@ -1,33 +1,15 @@
 # StatsOnShakespeare.py
 
-import numpy as np
-import pandas as pd
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+import networkx as nx
+
+import seaborn as sns
+import matplotlib.pyplot as plt 
+
 import streamlit as st
-import altair as alt
-from wordcloud import WordCloud, STOPWORDS
-import glob, nltk, os, re
-from nltk.corpus import stopwords 
-import nltk
-nltk.download('punkt')
-import string
-nltk.download('stopwords')
 
-# Create a dictionary (not a list)
-books = {" ":" ","A Mid Summer Night's Dream":"data/summer.txt","The Merchant of Venice":"data/merchant.txt","Romeo and Juliet":"data/romeo.txt"}
+# Dataset: https://www.kaggle.com/datasets/kingburrito666/shakespeare-plays?select=Shakespeare_data.csv
 
-## Select text files
-pickPlay = st.selectbox("Choose a text file", books.keys())
-
-## Get the value
-pickPlay = books.get(pickPlay)
-tab1, tab2, tab3 = st.tabs(['Word Cloud', 'Bar Chart', 'View Text'])
-
-if pickPlay != " ":
-    stop_words = []
-    raw_text = open(pickPlay,"r").read().lower()
-    nltk_stop_words = stopwords.words('english')
-    
-    stop_words = set(nltk_stop_words)
-    tokens = nltk.word_tokenize(raw_text)
-    
-    st.write(pickPlay)
+data = pd.read_csv('data/Shakespeare_data.csv', engine='python')
