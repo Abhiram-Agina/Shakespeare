f0049ffded882f49f7f5aec44a3251042c4ef22f
Abhiram-Agina <50762306+Abhiram-Agina@users.noreply.github.com>
2023-05-08 02:13:24 -0700

    Update StatsOnShakespeare.py

--- StatsOnShakespeare.py
+++ StatsOnShakespeare.py
@@ -13,5 +13,5 @@ import streamlit as st
 # Dataset Source: https://www.kaggle.com/datasets/kingburrito666/shakespeare-plays?select=Shakespeare_data.csv
 data = pd.read_csv('data/Shakespeare_data.csv', engine='python')
 
-data.info()
-data.head(5)
+st.write(data.info())
+st.write(data.head(5))
