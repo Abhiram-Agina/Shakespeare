a637f1a71502db378b660fa2434ef83cf6a7a7cd
Abhiram-Agina <50762306+Abhiram-Agina@users.noreply.github.com>
2023-05-08 01:34:46 -0700

    Update StatsOnShakespeare.py

--- StatsOnShakespeare.py
+++ StatsOnShakespeare.py
@@ -11,3 +11,23 @@ import nltk
 nltk.download('punkt')
 import string
 nltk.download('stopwords')
+
+# Create a dictionary (not a list)
+books = {" ":" ","A Mid Summer Night's Dream":"data/summer.txt","The Merchant of Venice":"data/merchant.txt","Romeo and Juliet":"data/romeo.txt"}
+
+## Select text files
+pickPlay = st.selectbox("Choose a text file", books.keys())
+
+## Get the value
+pickPlay = books.get(pickPlay)
+tab1, tab2, tab3 = st.tabs(['Word Cloud', 'Bar Chart', 'View Text'])
+
+if pickPlay != " ":
+    stop_words = []
+    raw_text = open(pickPlay,"r").read().lower()
+    nltk_stop_words = stopwords.words('english')
+    
+    stop_words = set(nltk_stop_words)
+    tokens = nltk.word_tokenize(raw_text)
+    
+    st.write(pickPlay)
