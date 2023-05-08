27c7c4abe172a9a7c28953023181f71ab7d1ebaf
Abhiram-Agina <50762306+Abhiram-Agina@users.noreply.github.com>
2023-05-08 02:16:43 -0700

    Update StatsOnShakespeare.py

--- StatsOnShakespeare.py
+++ StatsOnShakespeare.py
@@ -13,9 +13,12 @@ import streamlit as st
 # Dataset Source: https://www.kaggle.com/datasets/kingburrito666/shakespeare-plays?select=Shakespeare_data.csv
 data = pd.read_csv('data/Shakespeare_data.csv', engine='python')
 
-st.write("first 5 records:")
-st.write(data.head(5))
+st.title("Statistical Analysis of the Works of Shakespeare")
 
 add_selectbox = st.sidebar.selectbox(
-    "How would you like to be contacted?",
-    ("Email", "Home phone", "Mobile phone"))
+    "Please select an analysis:",
+    ("First 5", "Home phone", "Mobile phone"))
+
+with 'First 5':
+    st.write("first 5 records:")
+    st.write(data.head(5))
