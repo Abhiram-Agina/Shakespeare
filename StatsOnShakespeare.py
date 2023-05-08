42a6d608bf3f6590fa6ce244ae68f53953086978
Abhiram-Agina <50762306+Abhiram-Agina@users.noreply.github.com>
2023-05-08 02:25:40 -0700

    Update StatsOnShakespeare.py

--- StatsOnShakespeare.py
+++ StatsOnShakespeare.py
@@ -13,6 +13,9 @@ import streamlit as st
 # Dataset Source: https://www.kaggle.com/datasets/kingburrito666/shakespeare-plays?select=Shakespeare_data.csv
 data = pd.read_csv('data/Shakespeare_data.csv', engine='python')
 
+# replace 'NaN' with 'Other' in the Player column
+data['Player'].replace(np.nan, 'Other',inplace = True)
+
 st.title("Statistical Analysis of the Works of Shakespeare")
     
 nav = st.sidebar.radio("Stats",["First 5 Rows", "Chart2", "Chart3", "Chart4", "Chart5"])
