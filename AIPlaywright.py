4a7e4dce87e9892000945f85dc01629e3062a932
Abhiram-Agina <50762306+Abhiram-Agina@users.noreply.github.com>
2023-05-07 22:15:09 -0700

    Update AIPlaywright.py

--- AIPlaywright.py
+++ AIPlaywright.py
@@ -16,3 +16,21 @@ text = open(path_to_file, 'rb').read() #the entire corpus is now accessible via 
 text = text.decode(encoding='utf-8')
 st.write('Total number of characters in the corpus is:', len(text)) # returns 1115394
 st.write('The first 100 characters of the corpus are as follows:\n', text[:100])
+
+# Vectorize Text: STEP 1
+
+# To give each unique character an index number, we first have to find all the unique characters in the text file. 
+# This is very easy with the built-in set() function, which converts a list object to a set object.
+# lists are ordered and allow duplicates, while sets are unordered and do not allow duplicates. Lists and Sets are data structures.
+
+# The unique characters in the corpus
+vocab = sorted(set(text)) #set(text) function returns a set of unique characters in the text file
+st.write('The number of unique characters in the corpus is', len(vocab)) # returns 65 unique characters i.e. 26 alphabets, 10 numbers, punctuation, and special characters.
+st.write('unique characters set:\n', vocab[:]) #prints all 65 unique characters 
+
+# NOTE: in the above approach, RNN uses characters to generate new text. You could also create a vocab of unique words using the code below:
+import re
+words = re.findall('\w+', text.lower())
+uniq_words = sorted(set(words)) #set() finds unique words; sorted() converts the set into a sorted array
+len(uniq_words) #11,456 unique words
+st.write(uniq_words[:10]) # returns first 10 words from the 11,456 unique words
