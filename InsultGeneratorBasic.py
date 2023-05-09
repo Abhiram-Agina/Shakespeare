6b90c22bc058fedfbbba7f406c8f9e5be16c296a
Abhiram-Agina <50762306+Abhiram-Agina@users.noreply.github.com>
2023-05-08 23:34:01 -0700

    Update InsultGeneratorBasic.py

--- InsultGeneratorBasic.py
+++ InsultGeneratorBasic.py
@@ -6,10 +6,16 @@ import pandas as pd
 file = "FILES/Shakespeare_Insults - insults.csv"
 df = pd.read_csv(file)
 
-if st.button('Generate an Insult'):
-  rand_num = random.randint(1, (df.shape[0] - 1))
-  
-  st.write("Topic: ", str(df.iat[rand_num, 1]).upper())
-  st.write("Line: ", df.iat[rand_num, 2])
-  st.write("Act: ", df.iat[rand_num, 3])
-  st.write("Scene: ", str(df.iat[rand_num, 4]))
+col1, col2 = st.columns(3)
+with col1:
+  if st.button('Generate an Insult'):
+    rand_num = random.randint(1, (df.shape[0] - 1))
+
+    st.write("Topic: ", str(df.iat[rand_num, 1]).upper())
+    st.write("Line: ", df.iat[rand_num, 2])
+    st.write("Act: ", df.iat[rand_num, 3])
+    st.write("Scene: ", str(df.iat[rand_num, 4]))
+
+with col2:
+  st.write('second column')
+    
