87ebd592e8f7e292815de27b225f4d6e83653e20
Abhiram-Agina <50762306+Abhiram-Agina@users.noreply.github.com>
2023-05-08 23:45:35 -0700

    Update InsultGeneratorBasic.py

--- InsultGeneratorBasic.py
+++ InsultGeneratorBasic.py
@@ -1,4 +1,3 @@
-
 import random
 import streamlit as st
 import pandas as pd
@@ -8,6 +7,7 @@ df = pd.read_csv(file)
 
 col1, col2 = st.columns(2)
 with col1:
+  st.write("Bard's Insults")
   if st.button('Generate an Insult'):
     rand_num = random.randint(1, (df.shape[0] - 1))
 
@@ -17,5 +17,18 @@ with col1:
     st.write("Scene: ", str(df.iat[rand_num, 4]))
 
 with col2:
-  st.write('second column')
-    
+  st.subheader("Bot's Insults")
+  insultAdverbs = "FILES/adverbList.txt"
+  adverbs = pd.read_csv(insultAdverbs, sep = ' ')
+
+  insultAdjectives = "FILES/adjectiveList.txt"
+  adjectives = pd.read_csv(insultAdjectives, sep = ' ')
+
+  insultNouns = "FILES/nounList.txt"
+  nouns = pd.read_csv(insultNouns, sep = ' ')
+
+  if st.button('Generate a Wacky Shakespearean Insult'):
+    rand_num1 = random.randint(1, (adverbs.shape[0] - 1))
+    rand_num2 = random.randint(1, (adjectives.shape[0] - 1))
+    rand_num3 = random.randint(1, (nouns.shape[0] - 1))
+    st.write("Thou ", str(adverbs.iat[rand_num1, 0]).lower(), str(adjectives.iat[rand_num2, 0]).lower(), str(nouns.iat[rand_num3, 0]).lower())
