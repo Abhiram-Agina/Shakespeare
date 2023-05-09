038952c882e888f4bde8375cd941bfaffa352656
Abhiram-Agina <50762306+Abhiram-Agina@users.noreply.github.com>
2023-05-09 00:29:31 -0700

    Update SentimentDetectionL.py

--- SentimentDetectionL.py
+++ SentimentDetectionL.py
@@ -10,7 +10,7 @@ nltk.download('averaged_perceptron_tagger')
 st.title("Library-Based Sentiment Detection")
 
 st.markdown("[Click here for Shakespeare Sonnets](https://nosweatshakespeare.com/sonnets/)")
-st.markdown("[Or have Captain Kirk read them to you](https://www.youtube.com/hashtag/asonnetaday)")
+
 sonnetInput = st.text_area("Paste a Shakespearean Sonnet or PlayerLine here to analyze the sentiment:", height=300) 
 
 if st.button('Analyze Sentiment'):
