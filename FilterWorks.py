e1892ef48046f21a2ba997180339aba01d5c289b
Abhiram-Agina <50762306+Abhiram-Agina@users.noreply.github.com>
2023-05-09 00:33:30 -0700

    Update FilterWorks.py

--- FilterWorks.py
+++ FilterWorks.py
@@ -35,6 +35,8 @@ st.video('https://www.youtu.be/QTu39aMg_mU') # Why you should read Hamlet?
 
 st.write("""
   This program aims to serve students in their search for accurate quotations based on Play, Player, and Act/Scene/Line. 
+  
   I created this program by organizing which inputs a user might provide and filtering the data as necessary.
+  
   Data Source: Kaggle
 """)
