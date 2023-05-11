2279cf2f2d5c79f8c498f9102a747296fa15451a
Abhiram-Agina <50762306+Abhiram-Agina@users.noreply.github.com>
2023-05-10 23:37:11 -0700

    Update Linguistics.py

--- Linguistics.py
+++ Linguistics.py
@@ -3,6 +3,11 @@ import streamtlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
+import networkx as nx
+import seaborn as sns
+
+nav = st.sidebar.radio("Stats",["Letters", "Starting Letters", "Words"])
+
 #Inputting Text
 textInput = """Not marble, nor the gilded monuments
 Of princes, shall outlive this powerful rhyme;
@@ -24,47 +29,26 @@ letterList = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', '
 
 #Splitting Input
 tokenizedInput = (textInput.lower()).split()
-print(tokenizedInput)
+#print(tokenizedInput)
 
-#Counting Letters - TOTAL
-letterCounts = []
-counter = 0
-
-for match in letterList:
+if nav == "Letters":
+  #Counting Letters - TOTAL
+  letterCounts = []
   counter = 0
-  for token in tokenizedInput:
-    for letter in token:
-      if match == letter:
-        counter = counter + 1
-  letterCounts.append(counter)
 
-#print(letterList)
-#print(letterCounts)
+  for match in letterList:
+    counter = 0
+    for token in tokenizedInput:
+      for letter in token:
+        if match == letter:
+          counter = counter + 1
+    letterCounts.append(counter)
+    #print(letterList)
+    #print(letterCounts)
 
-#Crafting Graph - TOTAL
-plt.bar(letterList, letterCounts)
-plt.title('Most Used Letters - TOTAL')
-plt.xlabel('Letter')
-plt.ylabel('Frequency')
-plt.show()
-
-#Counting Letters - START
-startCounts = []
-counter = 0
-
-for match in letterList:
-  counter = 0
-  for token in tokenizedInput:
-    if match == token[0]:
-      counter = counter + 1
-  startCounts.append(counter)
-
-#print(letterList)
-print(startCounts)
-
-#Crafting Graph - START
-plt.bar(letterList, startCounts)
-plt.title('Most Used Letters - Starting')
-plt.xlabel('Letter')
-plt.ylabel('Frequency')
-plt.show()
+  #Crafting Graph - TOTAL
+  plt.title('Most Used Letters - TOTAL')  
+  plt.figure(figsize=(10,10))
+  ax = sns.barplot(x='letterList',y='letterCounts',data=textInput)
+  ax.set(xlabel='Letters', ylabel='Frequency')
+  st.pyplot()
